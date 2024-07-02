085e51a9b6b0ecf1ed18d4050c0582425713921c
Faris Munir <120432426+farismnrr@users.noreply.github.com>
2024-07-03 00:20:38 +0700

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,13 +5,13 @@ export const APP_NAME = 'Open WebUI';
 
 export const WEBUI_HOSTNAME = browser ? (dev ? `${location.hostname}:8080` : ``) : '';
 export const WEBUI_BASE_URL = browser ? (dev ? `http://${WEBUI_HOSTNAME}` : ``) : ``;
-export const WEBUI_API_BASE_URL = `${WEBUI_BASE_URL}/api/v1`;
+export const WEBUI_API_BASE_URL = `${WEBUI_BASE_URL}/api/v2`;
 
 export const OLLAMA_API_BASE_URL = `${WEBUI_BASE_URL}/ollama`;
 export const OPENAI_API_BASE_URL = `${WEBUI_BASE_URL}/openai`;
-export const AUDIO_API_BASE_URL = `${WEBUI_BASE_URL}/audio/api/v1`;
-export const IMAGES_API_BASE_URL = `${WEBUI_BASE_URL}/images/api/v1`;
-export const RAG_API_BASE_URL = `${WEBUI_BASE_URL}/rag/api/v1`;
+export const AUDIO_API_BASE_URL = `${WEBUI_BASE_URL}/audio/api/v2`;
+export const IMAGES_API_BASE_URL = `${WEBUI_BASE_URL}/images/api/v2`;
+export const RAG_API_BASE_URL = `${WEBUI_BASE_URL}/rag/api/v2`;
 
 export const WEBUI_VERSION = APP_VERSION;
 export const WEBUI_BUILD_HASH = APP_BUILD_HASH;
